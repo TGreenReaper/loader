e60dd0ab93184381a89c09b5310a1500dda11187
TGreenReaper <140297612+TGreenReaper@users.noreply.github.com>
2024-06-30 14:11:01 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,15 +11,13 @@ local TextLabel_2 = Instance.new("TextLabel")
 HUI.Name = "HUI"
 HUI.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-
 Main.Name = "Main"
 Main.Parent = HUI
-Main.BackgroundColor3 = Color3.fromRGB(126, 126, 126)
+Main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Main.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Main.BorderSizePixel = 0
 Main.Position = UDim2.new(0.0064620357, 0, 0.0177993532, 0)
 Main.Size = UDim2.new(0, 239, 0, 173)
-Main.Draggable = true;
 
 UICorner.Parent = Main
 
@@ -29,14 +27,14 @@ TextLabel.BackgroundTransparency = 1.000
 TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.BorderSizePixel = 0
 TextLabel.Size = UDim2.new(0, 239, 0, 59)
-TextLabel.Font = Enum.Font.Fondamento
+TextLabel.Font = Enum.Font.Bangers
 TextLabel.Text = "Reaper Loader"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 30.000
 
 divider.Name = "divider"
 divider.Parent = Main
-divider.BackgroundColor3 = Color3.fromRGB(49, 49, 49)
+divider.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 divider.BorderColor3 = Color3.fromRGB(0, 0, 0)
 divider.BorderSizePixel = 0
 divider.Position = UDim2.new(0, 0, 0.341040462, 0)
@@ -49,14 +47,16 @@ TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel_2.BorderSizePixel = 0
 TextLabel_2.Position = UDim2.new(0.079497911, 0, 0.549132943, 0)
 TextLabel_2.Size = UDim2.new(0, 200, 0, 50)
-TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Font = Enum.Font.LuckiestGuy
 TextLabel_2.Text = "Searching for game."
-TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
 TextLabel_2.TextSize = 30.000
+TextLabel_2.TextWrapped = true
 
 -- Scripts:
 
-local function DYZCH_fake_script() -- Main.LocalScript 
+local function OUPQDP_fake_script() -- Main.LocalScript 
 	local script = Instance.new('LocalScript', Main)
 
 	local ts = game:GetService("TweenService")
@@ -82,11 +82,11 @@ local function DYZCH_fake_script() -- Main.LocalScript
 		end
 	end))
 end
-coroutine.wrap(DYZCH_fake_script)()
-local function XZKYG_fake_script() -- TextLabel_2.LocalScript 
+coroutine.wrap(OUPQDP_fake_script)()
+local function OJBDAH_fake_script() -- TextLabel_2.LocalScript 
 	local script = Instance.new('LocalScript', TextLabel_2)
 
-	if game.PlaceId == 2753915549 then 
+	if game.PlaceId == 2753915549  or 7449423635 then 
 		task.wait(2)
 		script.Parent.Text = "Found Blox Fruits Script..."
 		task.wait(1)
@@ -101,7 +101,23 @@ local function XZKYG_fake_script() -- TextLabel_2.LocalScript
 		getgenv().Fix_Lag = true
 		getgenv().Auto_Execute = false
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/NGUYENVUDUY1/V3PRO/main/HiruHub.lua"))()
+		script.Parent.Parent.Visible = false
+	elseif game.PlaceId == 4490140733 then
+		task.wait(2)
+		script.Parent.Text = "Found My Resteraunt Script..."
+		task.wait(1)
+		script.Parent.Text = "Launching in"
+		task.wait(.5)
+		script.Parent.Text = "3"
+		task.wait(1)
+		script.Parent.Text = "2"
+		task.wait(1)
+		script.Parent.Text = "1"
+		task.wait(1)
+		script.Parent.Parent.Visible = false
+	else 
+		script.Parent.Text = "Script not found."
 	end
 	
 end
-coroutine.wrap(XZKYG_fake_script)()
+coroutine.wrap(OJBDAH_fake_script)()
